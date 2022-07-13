ce37a09272a66007b1060c944e6e47946a3f8172
Dheeraj <dheerajstvpm@gmail.com>
2022-07-13 10:17:19 +0530

    Update script.js

--- Day3/script.js
+++ Day3/script.js
@@ -8,7 +8,53 @@ function back(){
     document.getElementById("screen").value=document.getElementById("screen").value.slice(0,-1);
 }
 function equalClick(){
-    var text=document.getElementById("screen").value;
-    var result=eval(text);
-    document.getElementById("screen").value=result;
+    
+    if(flag==0){
+        var previousValue=document.getElementById("screen").value;
+    }
+    else if(flag==1){
+        var previousValue=previousValue+document.getElementById("screen").value;
+    }
+    else if(flag==2){
+        var previousValue=previousValue-document.getElementById("screen").value;
+    }
+    else if(flag==3){
+        var previousValue=previousValue*document.getElementById("screen").value;
+    }
+    else {
+        var previousValue=previousValue/document.getElementById("screen").value;
+    }
+    document.getElementById("screen").value=previousValue;
+}
+function operationClick(val){
+    if(document.getElementById("screen").value=='+'){
+        var flag=1;
+    }
+    if(document.getElementById("screen").value=='-'){
+        var flag=2;
+    }
+    if(document.getElementById("screen").value=='*'){
+        var flag=3;
+    }
+    else{
+        var flag=4;
+    }
+
+    if(flag==0){
+        var previousValue=document.getElementById("screen").value;
+    }
+    else if(flag==1){
+        var previousValue=previousValue+document.getElementById("screen").value;
+    }
+    else if(flag==2){
+        var previousValue=previousValue-document.getElementById("screen").value;
+    }
+    else if(flag==3){
+        var previousValue=previousValue*document.getElementById("screen").value;
+    }
+    else{
+        var previousValue=previousValue/document.getElementById("screen").value;
+    }
+
+    document.getElementById("screen").value="";
 }
