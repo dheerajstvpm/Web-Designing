7de169b8279df33c70f0763edddde1de33de05e3
Dheeraj <dheerajstvpm@gmail.com>
2022-07-15 18:48:14 +0530

    Update script.js

--- fb/script.js
+++ fb/script.js
@@ -17,14 +17,11 @@ $(document).ready(function(){
             },
             password:{
                 required:true,
-                maxlength:30,
-                minlength:8,
+                passwordCheck:true,
             },
             confirmPassword:{
                 required:true,
-                maxlength:30,
-                minlength:8,
-                equalTo:"#password-form",
+                equalTo:'#password-form',
             },
             day:{
                 required:true,
@@ -40,4 +37,29 @@ $(document).ready(function(){
             },
         }
     })
+    $.validator.addMethod("passwordCheck", function (value, element) {
+        let password = value;
+        if (!(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%&])(.{8,20}$)/.test(password))) {
+            return false;
+        }
+        return true;
+    }, function (value, element) {
+        let password = $(element).val();
+        if (!(/^(.{8,20}$)/.test(password))) {
+            return 'Password must be between 8 to 20 characters long.';
+        }
+        else if (!(/^(?=.*[A-Z])/.test(password))) {
+            return 'Password must contain at least one uppercase.';
+        }
+        else if (!(/^(?=.*[a-z])/.test(password))) {
+            return 'Password must contain at least one lowercase.';
+        }
+        else if (!(/^(?=.*[0-9])/.test(password))) {
+            return 'Password must contain at least one digit.';
+        }
+        else if (!(/^(?=.*[!@#$%^&*()-+=<>?])/.test(password))) {
+            return "Password must contain special characters from !@#$%^&*()-+=<>?";
+        }
+        return false;
+    });
 })
